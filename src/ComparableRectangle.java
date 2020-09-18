0a81307845cbd5ea3de777668dfff32a0c7ea11c
unknown <nguyenkimbao361@gmail.com>
2020-09-18 11:37:29 +0700

    add

---
+++ src/ComparableRectangle.java
@@ -0,0 +1,12 @@
+public class ComparableRectangle extends Rectangle {
+    public ComparableRectangle() {
+    }
+
+    public ComparableRectangle(double width, double length) {
+        super(width, length);
+    }
+
+    public ComparableRectangle(double width, double length, String color, boolean filled) {
+        super(width, length, color, filled);
+    }
+}
